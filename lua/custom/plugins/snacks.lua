1dadc735cb61b818a8e0894e45c3360f57143784
Moon Ji Hoon <variableruby@gmail.com>
2025-04-16 15:53:15 +0800

    Update dashboard

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -65,6 +65,15 @@ return {
         -- Check https://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=LAZYVIM
         -- for generating ASCII art
         preset = {
+          keys = {
+            { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
+            { icon = ' ', key = 'g', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = ' ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            { icon = ' ', key = 'c', desc = 'Config', action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+            { icon = '󰒲 ', key = 'L', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
+            { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+          },
           header = [[
            ▄█  ▄██   ▄    ▄█   ▄██████▄   ▄██████▄  
 ███  ███   ██▄ ███  ███    ███ ███    ███ ███▀▀▀██▄ 
