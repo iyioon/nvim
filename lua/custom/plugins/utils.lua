557af73cd02f78fad909c0890b3e8f0a6a6429bf
Moon Ji Hoon <variableruby@gmail.com>
2025-05-23 22:26:29 +0900

    Update autotag configuration

--- lua/custom/plugins/utils.lua
+++ lua/custom/plugins/utils.lua
@@ -46,10 +46,14 @@ return {
   {
     'windwp/nvim-ts-autotag',
     opts = {
-      -- Defaults
-      enable_close = true, -- Auto close tags
-      enable_rename = true, -- Auto rename pairs of tags
-      enable_close_on_slash = false, -- Auto close on trailing </
+      opts = {
+        enable_close = true, -- auto close tags
+        enable_rename = true, -- auto rename paired tags
+        enable_close_on_slash = false, -- don’t close on trailing `</`
+      },
+      per_filetype = {
+        -- ["html"] = { enable_close = false },
+      },
     },
   },
 
