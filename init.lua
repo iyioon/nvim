ee6ecad5de0e025cdb2fb410017b018cac0557f7
Moon Ji Hoon <variableruby@gmail.com>
2026-02-11 17:30:44 +0800

    Fix nvim-treesitter config for new API
    The nvim-treesitter.configs module was removed in a recent update.
    Updated to use the new API with require('nvim-treesitter').install()
    and vim.treesitter.start() as per upstream kickstart.nvim.

--- init.lua
+++ init.lua
@@ -1080,22 +1080,24 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-        disable = { 'tex', 'latex' }, -- Disable highlighting for LaTeX files (will be handled by vimtex)
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
+    config = function()
+      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+      local filetypes = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'latex' }
+      require('nvim-treesitter').install(filetypes)
+
+      -- Enable treesitter highlighting for supported filetypes
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = filetypes,
+        callback = function()
+          -- Don't enable for tex/latex if vimtex is handling it
+          local ft = vim.bo.filetype
+          if (ft == 'tex' or ft == 'latex') and vim.g.vimtex_enabled then
+            return
+          end
+          vim.treesitter.start()
+        end,
+      })
+    end,
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
     --
