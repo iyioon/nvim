86e6585f5feb6365e4dcf5570a4d43705545a2b1
Moon Ji Hoon <variableruby@gmail.com>
2026-03-11 23:16:01 +0800

    Add <leader>sc to search commands with Telescope

--- lua/plugins/editor/telescope.lua
+++ lua/plugins/editor/telescope.lua
@@ -44,6 +44,7 @@ return {
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
 
       -- Multigrep with filetype filtering
       -- Usage: search query, two spaces, then file glob pattern
